5261b9ec039c8147ce03b014bc06c799909fd231
zlaval <5099780+zlaval@users.noreply.github.com>
2025-12-15 21:17:36 +0000

    feat: Generate class Address

---
+++ src/main/java/com/zlrx/model/Address.java
@@ -0,0 +1,12 @@
+package com.zlrx.model;
+
+import java.lang.Long;
+import java.lang.String;
+
+public class Address {
+    private Long id;
+
+    private String citz;
+
+    private String street;
+}
