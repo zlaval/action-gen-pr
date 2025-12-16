e8c8519d2fb9442e5282990dd0c684bb49aa223d
zlaval <5099780+zlaval@users.noreply.github.com>
2025-12-16 09:00:21 +0000

    feat: Generate class NewClass

---
+++ src/main/java/com/example/model/NewClass.java
@@ -0,0 +1,13 @@
+package com.example.model;
+
+import java.lang.Integer;
+import java.lang.Long;
+import java.lang.String;
+
+public class NewClass {
+    private Long id;
+
+    private String name;
+
+    private Integer age;
+}
